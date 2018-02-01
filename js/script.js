ebec6088830a28846bdc4457b9496a371eb45a1f
andregouveia8 <afilipecgouveia@gmail.com>
2018-02-01 21:32:38 +0000

    update 01/02

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function () {
 
+
+
+
+
+
+
     $.ajax({
         url: "https://www.engadget.com/rss.xml",
         type: "GET"
@@ -14,10 +20,38 @@ $(function () {
             var title = lstNews[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
             var author = lstNews[i].getElementsByTagName("dc:creator")[0].childNodes[0].nodeValue;
             var creationDate = lstNews[i].getElementsByTagName("pubDate")[0].childNodes[0].nodeValue;
-            var description = lstNews[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-            var categories = lstNews[i].getElementsByTagName("category")[0].childNodes[0].nodeValue;
+            var description = lstNews[i].getElementsByTagName("description")[0].childNodes[0].wholeText;
+            var lstCategories = lstNews[i].getElementsByTagName("category");
             var linkToSee = lstNews[i].getElementsByTagName("link")[0].childNodes[0].nodeValue;
             var linkToComment = lstNews[i].getElementsByTagName("comments")[0].childNodes[0].nodeValue;
+            var tempDescription = description.split("/>");
+            var linkImg = tempDescription[0];
+            var newsDescription = tempDescription[1];
+
+            var creationDateTemp = creationDate.split("-");
+            var creationDateUpdated = creationDateTemp[0];
+
+            var categories = [];
+
+            for (j = 0; j < lstCategories.length; j++) {
+                categoryTemp = lstCategories[j].childNodes[0].nodeValue;
+                categories.push(categoryTemp);
+
+            }
+
+
+
+
+
+            console.log(categories)
+
+
+            var buttonSee = "<a class='button' target='_blanck'href='" + linkToSee + "'>See on Engadget </a>";
+            var buttonComment = "<a class='button' target='_blanck' href='" + linkToComment + "'>Comment the post</a>";
+
+
+            var html = "<br><div class='card'><div class='card-header'><a href='" + linkToSee + "' target='_blank'><h1>" + title + "</h1></a><p4>" + creationDateUpdated + " posted by " + author + "</p4></div><div class='card-block'><div class='row'>    <div class='col-md-6' ><br><div class='col-md-12'><p1>" + newsDescription + "</p1></div><div class='col-md-12 center'><br><br><div class='col-md-6 center'>" + buttonSee + "</div><br><div class='col-md-6 center'>" + buttonComment + "</div></div></div><div class='col-md-6 center'>" + linkImg + "</p1></div></div></div ></div >"
+            $("#news").append(html);
 
             if (i == 9) {
                 break;
