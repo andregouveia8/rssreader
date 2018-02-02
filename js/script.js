d7f8f79dbc9cbf1629ecc39450890130c5c92839
andregouveia8 <afilipecgouveia@gmail.com>
2018-02-02 14:14:33 +0000

    lastUpdate

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 $(function () {
-
-
-
-
-
-
-
+    var theme;
+    var getCSS = true;
     $.ajax({
         url: "https://www.engadget.com/rss.xml",
-        type: "GET"
+        type: "GET",
+        crossDomain: true
     }).done(function (data, textStatus) {
-
         console.log(data);
         var lstNews = data.getElementsByTagName("item");
-        console.log(lstNews);
 
         for (i = 0; i < lstNews.length; i++) {
 
@@ -34,23 +28,13 @@ $(function () {
             var categories = [];
 
             for (j = 0; j < lstCategories.length; j++) {
-                categoryTemp = lstCategories[j].childNodes[0].nodeValue;
+                categoryTemp = " " + lstCategories[j].childNodes[0].nodeValue;
                 categories.push(categoryTemp);
 
             }
-
-
-
-
-
-            console.log(categories)
-
-
             var buttonSee = "<a class='button' target='_blanck'href='" + linkToSee + "'>See on Engadget </a>";
             var buttonComment = "<a class='button' target='_blanck' href='" + linkToComment + "'>Comment the post</a>";
-
-
-            var html = "<br><div class='card'><div class='card-header'><a href='" + linkToSee + "' target='_blank'><h1>" + title + "</h1></a><p4>" + creationDateUpdated + " posted by " + author + "</p4></div><div class='card-block'><div class='row'>    <div class='col-md-6' ><br><div class='col-md-12'><p1>" + newsDescription + "</p1></div><div class='col-md-12 center'><br><br><div class='col-md-6 center'>" + buttonSee + "</div><br><div class='col-md-6 center'>" + buttonComment + "</div></div></div><div class='col-md-6 center'>" + linkImg + "</p1></div></div></div ></div >"
+            var html = "<br><div class='card'><div class='card-header'><a href='" + linkToSee + "' target='_blank'><h1>" + title + "</h1></a><p4>" + creationDateUpdated + " posted by " + author + "</p4></div><div class='card-block'><div class='row'>    <div class='col-md-6' ><br><div class='col-md-12'><p1>" + newsDescription + "</p1></div><div class='col-md-12 center'><br><br><div class='col-md-6 center'>" + buttonSee + "</div><br><div class='col-md-6 center'>" + buttonComment + "</div></div></div><div class='col-md-6 center'>" + linkImg + "</p1></div></div></div ><div class='card-footer center'><p2>" + categories + "</p2></div></div >"
             $("#news").append(html);
 
             if (i == 9) {
@@ -58,23 +42,29 @@ $(function () {
             }
 
         }
-
-
-
-
-
-
     }).fail(function (xhr, status, errorThrown) {
-        // alert("Surgiu um problema!");
-        console.log("Erro: " + errorThrown);
-        console.log("Estado: " + status);
-        console.dir(xhr);
+        alert("To view this page, please install the 'Allow-Control-Allow-Origin' extension of Google Chrome! Thank you!");
     })
 
+    // Button Click to Change Theme
+
+    $("#btnTheme").click(function () {
+
+        if ($('link[href="css/white.css"]').attr("href") == undefined) {
+            theme = false;
+        } else {
+            theme = true;
+        }
 
 
-
-
+        if (theme == true) {
+            $('link[href="css/white.css"]').attr('href', 'css/dark.css');
+            theme == false;
+        } else {
+            $('link[href="css/dark.css"]').attr('href', 'css/white.css');
+            theme == true;
+        }
+    });
 
 
 
